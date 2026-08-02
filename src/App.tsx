d87f4550909d1a873b5979f2d0cef01c617870a5
Manas Pant <manasmonupant@gmail.com>
2026-08-02 19:36:57 +0530

    feat: initial raw html

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,34 @@ import './App.css'
 function App() {
   return (
     <>
-      <section id="center">      
-      </section>
+    <header>
+      DevCompass
+      <p>"Level Up Your Learning Journey"</p>
+      <p>Profile</p>
+      </header>
+      <main>
+        <section>
+        Welcome back Manas!
+        <p>Your next quest</p>Data here
+        </section>
+        <section>
+          <p>Card1 Level</p>
+          <p>Card2 Streak</p>
+          <p>Card3 XP earned</p>
+        </section>
+        <section>
+          Progress chart here(Everything from data0)
+        </section>
+        <section>
+          Choose technology
+          <p>Different selection card1 here</p>
+          <p>Different selection card2 here</p>
+          <p>Different selection card3 here</p>
+          <p>Different selection card4 here</p>
+        </section>
+      </main>
     </>
+
   )
 }
 
